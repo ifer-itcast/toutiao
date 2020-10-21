c24f30f42763c750d664fbfed729c654ab0c0379
ifer-itcast <yangkangkang@itcast.cn>
2020-10-21 23:22:27 +0800

    使用Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h1>test</h1>
     <i class="iconfont iconfenxiang"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+// Vant 的样式
+import 'vant/lib/index.css'
 // 全局样式
 import './styles/index.less'
-
+Vue.use(Vant)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,

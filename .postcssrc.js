ced8db7b4413bdfcc8ca5e5c86eb2522a4fcac9f
ifer-itcast <yangkangkang@itcast.cn>
2020-10-21 23:50:49 +0800

    REM适配配置

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    // Vue-cli 已经配置了
+    // 'autoprefixer': {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
+    'postcss-pxtorem': {
+      rootValue ({ file }) {
+        // file => 要编译的样式的路径
+        return file.includes('vant') ? 37.5 : 75
+      },
+      propList: ['*']
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import Vant from 'vant'
 import 'vant/lib/index.css'
 // 全局样式
 import './styles/index.less'
+// 动态设置根元素字体大小
+import 'amfe-flexible'
+
 Vue.use(Vant)
 Vue.config.productionTip = false
 new Vue({

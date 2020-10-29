15067b00ce6a574ec27748a32507fe0e456ef085
ifer-itcast <yangkangkang@itcast.cn>
2020-10-30 00:53:50 +0800

    个人中心顶部未登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="login-container">
     <!-- 头部 -->
-    <van-nav-bar class="page-nav-bar" title="登录" />
+    <van-nav-bar class="page-nav-bar" title="登录">
+      <van-icon slot="left" name="cross" @click="$router.back()" />
+    </van-nav-bar>
     <!-- 表单 -->
     <van-form @submit="onSubmit" ref="loginForm">
       <!-- 手机号 -->
--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="my-container">我的</div>
+  <div class="my-container">
+    <!-- 未登录头部 -->
+    <div class="header not-login">
+      <div class="login-btn" @click="$router.push('/login')">
+        <img class="mobile-img" src="~@/assets/mobile.png" alt="" />
+        <span class="text">登录 / 注册</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +15,104 @@ export default {
   name: 'MyIndex'
 }
 </script>
+
+<style lang="less" scoped>
+.my-container {
+  .header {
+    height: 361px;
+    background: url('~@/assets/banner.png');
+    background-size: cover;
+  }
+
+  .not-login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-btn {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .mobile-img {
+        width: 132px;
+        height: 132px;
+        margin-bottom: 15px;
+      }
+      .text {
+        font-size: 28px;
+        color: #fff;
+      }
+    }
+  }
+
+  .user-info {
+    .base-info {
+      height: 231px;
+      padding: 76px 32px 23px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 132px;
+          height: 132px;
+          border: 4px solid #fff;
+          margin-right: 23px;
+        }
+        .name {
+          font-size: 30px;
+          color: #fff;
+        }
+      }
+    }
+    .data-stats {
+      display: flex;
+      .data-item {
+        height: 130px;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size: 23px;
+        }
+      }
+    }
+  }
+
+  .grid-nav {
+    .grid-item {
+      height: 141px;
+      i.toutiao {
+        font-size: 45px;
+      }
+      .toutiao-shoucang {
+        color: #eb5253;
+      }
+      .toutiao-lishi {
+        color: #ff9d1d;
+      }
+      span.text {
+        font-size: 28px;
+      }
+    }
+  }
+
+  .logout-cell {
+    text-align: center;
+    color: #d86262;
+  }
+
+  .mb-9 {
+    margin-bottom: 9px;
+  }
+}
+</style>

4b62dd58e3f990257deeb14fb5a1c96bbdbc697a
ifer-itcast <yangkangkang@itcast.cn>
2020-10-31 21:42:06 +0800

    使用Tab标签页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -12,12 +12,24 @@
         >搜索</van-button
       >
     </van-nav-bar>
+    <!-- 频道列表 -->
+    <van-tabs v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIndex'
+  name: 'HomeIndex',
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 

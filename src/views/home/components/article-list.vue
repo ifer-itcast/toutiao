8d0f5f1b227c1b6e93fddf09cb54f101e8b87121
ifer-itcast <yangkangkang@itcast.cn>
2020-11-01 00:14:30 +0800

    下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="article-list">
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      :error.sync="error"
-      error-text="请求失败，点击重新加载"
-      @load="onLoad"
+    <van-pull-refresh
+      v-model="isRefreshLoading"
+      @refresh="onRefresh"
+      :success-text="refreshSuccessText"
+      :success-duration="1500"
     >
-      <van-cell
-        v-for="(article, index) in list"
-        :key="index"
-        :title="article.title"
-      />
-    </van-list>
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
+        @load="onLoad"
+      >
+        <van-cell
+          v-for="(article, index) in list"
+          :key="index"
+          :title="article.title"
+        />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -33,7 +40,9 @@ export default {
       loading: false, // 控制加载中 loading 状态，默认不 loading
       finished: false, // 数据是否加载完成
       timestamp: null, // 请求获取下一页数据的时间戳
-      error: false // 控制列表失败的提示状态
+      error: false, // 控制列表失败的提示状态
+      isRefreshLoading: false, // 控制下拉刷新的 loading 状态
+      refreshSuccessText: '刷新成功' // 下拉刷新成功提示文本
     }
   },
   methods: {
@@ -69,6 +78,31 @@ export default {
         this.error = true
         // 请求失败了，loading 也需要关闭
         this.loading = false
+      }
+    },
+    async onRefresh() {
+      try {
+        // 请求获取数据
+        const { data } = await getArticles({
+          channel_id: this.channel.id,
+          timestamp: Date.now(),
+          with_top: 1
+        })
+        /* if (Math.random() > 0.5) {
+          JSON.parse('xxx')
+        } */
+        // 将数据追加到列表的顶部
+        const { results } = data.data
+        this.list.unshift(...results)
+        // 关闭下拉刷新的 loading 状态
+        this.isRefreshLoading = false
+        // 更新下拉刷新成功提示的文本
+        this.refreshSuccessText = `刷新成功，更新了${results.length}条数据`
+      } catch (err) {
+        // 关闭下拉刷新的 loading 状态
+        this.isRefreshLoading = false
+        // 更新下拉刷新成功提示的文本
+        this.refreshSuccessText = '刷新失败'
       }
     }
   }

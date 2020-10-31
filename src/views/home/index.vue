c8dd32db8a35e582f3bad21c951ce4f753e4c383
ifer-itcast <yangkangkang@itcast.cn>
2020-10-31 21:33:26 +0800

    头部导航栏布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home-container">主页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
@@ -7,3 +20,22 @@ export default {
   name: 'HomeIndex'
 }
 </script>
+
+<style lang="less" scoped>
+// 当前组件中加了 scoped 对内部样式的修改需要加 /deep/，或者去掉 scoped
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
+</style>

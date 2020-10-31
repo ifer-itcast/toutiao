a14cc4510725bb09bca3202cf547ff94272e9da7
ifer-itcast <yangkangkang@itcast.cn>
2020-10-31 22:06:27 +0800

    处理汉堡按钮

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -23,6 +23,10 @@
       <van-tab title="标签 4">内容 4</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="iconfont icongengduo"></i>
+      </div>
     </van-tabs>
   </div>
 </template>
@@ -77,6 +81,37 @@ export default {
       width: 31px !important;
       height: 6px;
       background-color: #3296fa;
+    }
+
+    // 汉堡
+    .placeholder {
+      flex-shrink: 0;
+      width: 66px;
+      height: 82px;
+    }
+
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 66px;
+      height: 82px;
+      background-color: #fff;
+      background-color: rgba(255, 255, 255, 0.902);
+      i.iconfont {
+        font-size: 33px;
+      }
+      &:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 58px;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
     }
   }
 }

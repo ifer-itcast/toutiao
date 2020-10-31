54f90f6ad8dab0ad302ea0d3976292d4fa78da2c
ifer-itcast <yangkangkang@itcast.cn>
2020-11-01 00:01:11 +0800

    请求失败处理

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -4,6 +4,8 @@
       v-model="loading"
       :finished="finished"
       finished-text="没有更多了"
+      :error.sync="error"
+      error-text="请求失败，点击重新加载"
       @load="onLoad"
     >
       <van-cell
@@ -30,7 +32,8 @@ export default {
       list: [], // 存储列表数据的数组
       loading: false, // 控制加载中 loading 状态，默认不 loading
       finished: false, // 数据是否加载完成
-      timestamp: null // 请求获取下一页数据的时间戳
+      timestamp: null, // 请求获取下一页数据的时间戳
+      error: false // 控制列表失败的提示状态
     }
   },
   methods: {
@@ -44,6 +47,10 @@ export default {
           timestamp: Date.now(),
           with_top: 1 // 是否包含置顶
         })
+        // 模拟随机失败的状态
+        /* if (Math.random() > 0.5) {
+          JSON.parse('xxx')
+        } */
         // 2. 把请求结果数据放到 list 数组中
         const { results } = data.data
         this.list.push(...results)
@@ -57,7 +64,12 @@ export default {
           // 没有数据了
           this.finished = true
         }
-      } catch (err) {}
+      } catch (err) {
+        // 展示错误提示状态
+        this.error = true
+        // 请求失败了，loading 也需要关闭
+        this.loading = false
+      }
     }
   }
 }

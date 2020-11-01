81229fea2d3e838852c172fc2a8fbf0790f31395
ifer-itcast <yangkangkang@itcast.cn>
2020-11-02 00:00:59 +0800

    频道编辑界面布局

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="channel-edit">
-    频道编辑
+    <!-- 我的频道标题 -->
+    <van-cell :border="false">
+      <div slot="title">我的频道</div>
+      <van-button class="edit-btn" type="danger" plain round size="mini"
+        >编辑</van-button
+      >
+    </van-cell>
+    <!-- 我的频道内容 -->
+    <van-grid class="my-grid" :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        v-for="(value, index) in 8"
+        :key="index"
+        text="文字"
+      >
+      </van-grid-item>
+    </van-grid>
+    <!-- 频道推荐标题 -->
+    <van-cell :border="false">
+      <div slot="title">频道推荐</div>
+    </van-cell>
+    <!-- 频道推荐内容 -->
+    <van-grid class="my-grid" :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        v-for="(value, index) in 8"
+        :key="index"
+        text="文字"
+      >
+      </van-grid-item>
+    </van-grid>
   </div>
 </template>
 
@@ -10,4 +40,20 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.channel-edit {
+  padding: 85px 0;
+  .title-text {
+    font-size: 32px;
+    color: #333333;
+  }
+
+  .edit-btn {
+    width: 104px;
+    height: 48px;
+    font-size: 26px;
+    color: #f85959;
+    border: 1px solid #f85959;
+  }
+}
+</style>

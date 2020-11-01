8d2d9a2e8498c658397193781aa6aa1e4e812c49
ifer-itcast <yangkangkang@itcast.cn>
2020-11-01 22:54:29 +0800

    根据每一个article.cover.type展示列表项内容

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="article-item">文章列表项</div>
+  <van-cell class="article-item">
+    <div slot="title" class="title">{{ article.title }}</div>
+    <div slot="label">
+      <!-- 三张图片的情况 -->
+      <div v-if="article.cover.type === 3" class="cover-wrap">
+        <div
+          class="cover-item"
+          v-for="(img, index) in article.cover.images"
+          :key="index"
+        >
+          <van-image width="100" height="100" :src="img" />
+        </div>
+      </div>
+      <!-- 文章信息 -->
+      <div>
+        <span>{{ article.aut_name }}</span>
+        <span>{{ article.comm_count }}评论</span>
+        <span>{{ article.pubdate }}</span>
+      </div>
+    </div>
+    <!-- 右侧一张图片的情况 -->
+    <van-image
+      v-if="article.cover.type === 1"
+      slot="default"
+      width="100"
+      height="100"
+      :src="article.cover.images[0]"
+    />
+  </van-cell>
 </template>
 
 <script>
@@ -14,6 +42,4 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>

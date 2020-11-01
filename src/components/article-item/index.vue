8e9d07fef9ac0a110555ed5a615faaf16db00e98
ifer-itcast <yangkangkang@itcast.cn>
2020-11-01 23:37:13 +0800

    处理相对时间

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -18,7 +18,7 @@
       <div class="label-info-wrap">
         <span>{{ article.aut_name }}</span>
         <span>{{ article.comm_count }}评论</span>
-        <span>{{ article.pubdate }}</span>
+        <span>{{ article.pubdate | relativeTime }}</span>
       </div>
     </div>
     <!-- 右侧一张图片的情况 -->
--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -1,7 +1,18 @@
+import Vue from 'vue'
 import dayjs from 'dayjs'
 // 加载中文语言包
 import 'dayjs/locale/zh-cn'
+
+import relativeTime from 'dayjs/plugin/relativeTime'
+// 配置使用处理相对时间的插件
+dayjs.extend(relativeTime)
+
 // dayjs 默认语言是中文，这里全局配置为中文
 dayjs.locale('zh-cn')
 
-console.log(dayjs().format('YYYY-MM-DD'))
+// 定义一个全局过滤器，然后就可以在任何组件的模板中使用了，相当于一个全局的仅供模板使用的方法
+// {{表达式|过滤器名称}}
+Vue.filter('relativeTime', value => {
+  return dayjs().to(dayjs(value))
+})
+// console.log(dayjs().format('YYYY-MM-DD'))

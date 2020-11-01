8ae8c3ed63ba84bcd6034e719a0d621ca2bdcb3f
ifer-itcast <yangkangkang@itcast.cn>
2020-11-01 23:13:03 +0800

    文章列表项样式调整

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,6 +1,8 @@
 <template>
   <van-cell class="article-item">
-    <div slot="title" class="title">{{ article.title }}</div>
+    <div slot="title" class="title van-multi-ellipsis--l2">
+      {{ article.title }}
+    </div>
     <div slot="label">
       <!-- 三张图片的情况 -->
       <div v-if="article.cover.type === 3" class="cover-wrap">
@@ -9,11 +11,11 @@
           v-for="(img, index) in article.cover.images"
           :key="index"
         >
-          <van-image width="100" height="100" :src="img" />
+          <van-image :src="img" fit="cover" class="cover-item-img" />
         </div>
       </div>
       <!-- 文章信息 -->
-      <div>
+      <div class="label-info-wrap">
         <span>{{ article.aut_name }}</span>
         <span>{{ article.comm_count }}评论</span>
         <span>{{ article.pubdate }}</span>
@@ -21,10 +23,9 @@
     </div>
     <!-- 右侧一张图片的情况 -->
     <van-image
+      class="right-cover"
       v-if="article.cover.type === 1"
       slot="default"
-      width="100"
-      height="100"
       :src="article.cover.images[0]"
     />
   </van-cell>
@@ -42,4 +43,51 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.article-item {
+  .title {
+    font-size: 32px;
+    color: #3a3a3a;
+  }
+
+  .van-cell__title {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  // 右侧的一张图片
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+    padding-left: 25px;
+  }
+
+  .right-cover {
+    width: 100%;
+    height: 146px;
+  }
+
+  .label-info-wrap span {
+    font-size: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0;
+    .cover-item {
+      flex: 1;
+      height: 146px;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+}
+</style>

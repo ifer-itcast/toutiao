2f5ccd32193f146a380a0be6037ef0da590dafb2
ifer-itcast <yangkangkang@itcast.cn>
2020-11-02 00:14:51 +0800

    页面样式

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -2,7 +2,7 @@
   <div class="channel-edit">
     <!-- 我的频道标题 -->
     <van-cell :border="false">
-      <div slot="title">我的频道</div>
+      <div slot="title" class="title-text">我的频道</div>
       <van-button class="edit-btn" type="danger" plain round size="mini"
         >编辑</van-button
       >
@@ -14,19 +14,21 @@
         v-for="(value, index) in 8"
         :key="index"
         text="文字"
+        icon="clear"
       >
       </van-grid-item>
     </van-grid>
     <!-- 频道推荐标题 -->
     <van-cell :border="false">
-      <div slot="title">频道推荐</div>
+      <div slot="title" class="title-text">频道推荐</div>
     </van-cell>
     <!-- 频道推荐内容 -->
-    <van-grid class="my-grid" :gutter="10">
+    <van-grid class="recommend-grid" :gutter="10">
       <van-grid-item
         class="grid-item"
         v-for="(value, index) in 8"
         :key="index"
+        icon="plus"
         text="文字"
       >
       </van-grid-item>
@@ -47,13 +49,58 @@ export default {
     font-size: 32px;
     color: #333333;
   }
-
+  // 编辑按钮
   .edit-btn {
     width: 104px;
     height: 48px;
     font-size: 26px;
     color: #f85959;
     border: 1px solid #f85959;
+  }
+  /deep/ .grid-item {
+    width: 160px;
+    height: 86px;
+    .van-grid-item__content {
+      white-space: nowrap;
+      background-color: #f4f5f6;
+      .van-grid-item__text,
+      .text {
+        font-size: 28px;
+        color: #222;
+        margin-top: 0;
+      }
+      .active {
+        color: red;
+      }
+      .van-grid-item__icon-wrapper {
+        position: unset;
+      }
+    }
+  }
+
+  /deep/ .my-grid {
+    .grid-item {
+      .van-icon-clear {
+        position: absolute;
+        right: -10px;
+        top: -10px;
+        font-size: 30px;
+        color: #cacaca;
+        z-index: 2;
+      }
+    }
+  }
+
+  /deep/ .recommend-grid {
+    .grid-item {
+      .van-grid-item__content {
+        flex-direction: row;
+        .van-icon-plus {
+          font-size: 28px;
+          margin-right: 6px;
+        }
+      }
+    }
   }
 }
 </style>

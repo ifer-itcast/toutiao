a2f2eb7f54699e60f5007fae152e9d141deaf189
ifer-itcast <yangkangkang@itcast.cn>
2020-11-03 13:09:36 +0800

    点击取消返回

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,13 +1,42 @@
 <template>
-  <div class="search-container">搜索页面</div>
+  <div class="search-container">
+    <!-- 搜索栏 -->
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        placeholder="请输入搜索关键词"
+        background="#3296fa"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchIndex'
+  name: 'SearchIndex',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSearch (val) {
+      console.log(val)
+    },
+    onCancel () {
+      this.$router.back()
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.search-container {
+  .van-search__action {
+    color: #fff;
+  }
+}
 </style>

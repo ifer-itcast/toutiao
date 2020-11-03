9710b75ddd1ab5fba05b4ebcda8a5557a33d5977
ifer-itcast <yangkangkang@itcast.cn>
2020-11-04 00:51:10 +0800

    完成点赞逻辑

--- src/api/article.js
+++ src/api/article.js
@@ -36,3 +36,22 @@ export const deleteCollect = target => {
     url: `/app/v1_0/article/collections/${target}`
   })
 }
+
+// 点赞文章
+export const addLike = target => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/article/likings',
+    data: {
+      target
+    }
+  })
+}
+
+// 取消点赞文章
+export const deleteLike = target => {
+  return request({
+    method: 'DELETE',
+    url: `/app/v1_0/article/likings/${target}`
+  })
+}
--- src/components/like-article/index.vue
+++ src/components/like-article/index.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import { addLike, deleteLike } from '@/api/article'
+
 export default {
   name: 'LikeArticle',
   data() {
@@ -21,10 +23,33 @@ export default {
     value: {
       type: Number,
       required: true
+    },
+    articleId: {
+      type: [Number, String, Object],
+      required: true
     }
   },
   methods: {
-    onCollect() {}
+    async onCollect() {
+      this.loading = true
+      try {
+        let status = -1
+        if (this.value === 1) {
+          // 已点赞，取消点赞
+          await deleteLike(this.articleId)
+        } else {
+          // 没有点赞，添加点赞
+          await addLike(this.articleId)
+          status = 1
+        }
+        this.$emit('input', status)
+        // 更新视图
+        this.$toast.success(status === 1 ? '点赞成功' : '取消点赞')
+      } catch (err) {
+        this.$toast.fail('操作失败，请重试！')
+      }
+      this.loading = false
+    }
   }
 }
 </script>

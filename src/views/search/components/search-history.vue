a0a2102928e0a18bdf9e8884f52138715f5ab82e
ifer-itcast <yangkangkang@itcast.cn>
2020-11-03 16:00:12 +0800

    处理删除按钮的展示状态

--- src/views/search/components/search-history.vue
+++ src/views/search/components/search-history.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="search-history">
     <van-cell title="搜索历史">
-      <span>全部删除</span>
-      <span>完成</span>
-      <van-icon name="delete"></van-icon>
+      <!-- isDeleteShow 默认为 false，即默认显示 v-else 的删除按钮 -->
+      <div v-if="isDeleteShow">
+        <span>全部删除</span>&nbsp;&nbsp;
+        <span @click="isDeleteShow = false">完成</span>
+      </div>
+      <van-icon v-else name="delete" @click="isDeleteShow = true"></van-icon>
     </van-cell>
     <van-cell
       :title="item"
       v-for="(item, index) in searchHistories"
       :key="index"
     >
-      <van-icon name="close" />
+      <!-- 每一行的删除按钮 -->
+      <van-icon v-show="isDeleteShow" name="close" />
     </van-cell>
   </div>
 </template>
@@ -22,6 +26,11 @@ export default {
     searchHistories: {
       type: Array,
       required: true
+    }
+  },
+  data() {
+    return {
+      isDeleteShow: false // 控制删除显示状态
     }
   }
 }

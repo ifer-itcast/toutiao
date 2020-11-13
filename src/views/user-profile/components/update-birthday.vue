d701298ef97b8a4173a8fe36e0859ab4db5ea0d1
ifer-itcast <yangkangkang@itcast.cn>
2020-11-13 17:29:07 +0800

    修改生日

--- src/views/user-profile/components/update-birthday.vue
+++ src/views/user-profile/components/update-birthday.vue
@@ -1,22 +1,76 @@
 <template>
-  <div>
-    修改生日
+  <div class="update-birthday">
+    <!--
+      currentDate 双向绑定了日期选择器
+        设置日期选择器的默认值
+        同步日期选择器选择的日期
+      min-date: 可选的最小日期
+      max-date: 可选的最大日期
+     -->
+    <van-datetime-picker
+      v-model="currentDate"
+      type="date"
+      :min-date="minDate"
+      :max-date="maxDate"
+      @cancel="$emit('close')"
+      @confirm="onConfirm"
+    />
   </div>
 </template>
 
 <script>
-export default {
-  name: '',
+import { updateUserProfile } from '@/api/user'
+import dayjs from 'dayjs'
 
-  data () {
-    return {
+export default {
+  name: 'UpdateBirthday',
+  components: {},
+  props: {
+    value: {
+      type: String,
+      required: true
     }
   },
+  data() {
+    return {
+      minDate: new Date(1970, 0, 1),
+      maxDate: new Date(),
+      currentDate: new Date(this.value)
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    async onConfirm() {
+      this.$toast.loading({
+        message: '保存中...',
+        forbidClick: true, // 禁止背景点击
+        duration: 0 // 持续展示
+      })
 
-  methods: {}
+      try {
+        const currentDate = dayjs(this.currentDate).format('YYYY-MM-DD')
+
+        await updateUserProfile({
+          birthday: currentDate
+        })
+
+        // 更新视图
+        this.$emit('input', currentDate)
+
+        // 关闭弹层
+        this.$emit('close')
+
+        // 提示成功
+        this.$toast.success('更新成功')
+      } catch (err) {
+        this.$toast.fail('更新失败')
+      }
+    }
+  }
 }
 </script>
 
-<style lang='stylus' rel='stylesheet/stylus'>
-
-</style>
+<style scoped lang="less"></style>

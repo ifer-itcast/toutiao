65f01f37bc1bfac4056331bfcddd74cfc983cc36
ifer-itcast <yangkangkang@itcast.cn>
2020-11-13 15:51:51 +0800

    页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    x
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="page-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+    <!-- 个人信息 -->
+    <van-cell title="头像" is-link>
+      <van-image
+        class="avatar"
+        fit="cover"
+        round
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" value="内容" is-link />
+    <van-cell title="性别" value="内容" is-link />
+    <van-cell title="生日" value="内容" is-link />
+    <!-- /个人信息 -->
   </div>
 </template>
 
@@ -8,15 +28,23 @@
 export default {
   name: '',
 
-  data () {
-    return {
-    }
+  data() {
+    return {}
   },
 
   methods: {}
 }
 </script>
 
-<style lang='stylus' rel='stylesheet/stylus'>
+<style lang="less" scoped>
+.user-profile {
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
 
+  .van-popup {
+    background-color: #f5f7f9;
+  }
+}
 </style>

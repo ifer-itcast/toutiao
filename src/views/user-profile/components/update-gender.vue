b45dd46d2c6c6e748987e6e73687c4a2f7a8ead6
ifer-itcast <yangkangkang@itcast.cn>
2020-11-13 17:13:40 +0800

    修改性别

---
+++ src/views/user-profile/components/update-gender.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="update-gender">
+    <van-picker
+      show-toolbar
+      title="标题"
+      :columns="columns"
+      :default-index="value"
+      @cancel="$emit('close')"
+      @confirm="onConfirm"
+      @change="onPickerChange"
+    />
+  </div>
+</template>
+
+<script>
+import { updateUserProfile } from '@/api/user'
+
+export default {
+  name: 'UpdateGender',
+  components: {},
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      columns: ['男', '女'],
+      localGender: this.value
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    async onConfirm() {
+      this.$toast.loading({
+        message: '保存中...',
+        forbidClick: true, // 禁止背景点击
+        duration: 0 // 持续展示
+      })
+
+      try {
+        const localGender = this.localGender
+
+        await updateUserProfile({
+          gender: localGender
+        })
+
+        // 更新视图
+        this.$emit('input', localGender)
+
+        // 关闭弹层
+        this.$emit('close')
+
+        // 提示成功
+        this.$toast.success('更新成功')
+      } catch (err) {
+        this.$toast.fail('更新失败')
+      }
+    },
+
+    onPickerChange(picker, value, index) {
+      this.localGender = index
+    }
+  }
+}
+</script>
+
+<style scoped lang="less"></style>
--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -18,7 +18,12 @@
       is-link
       @click="isUpdateNameShow = true"
     />
-    <van-cell title="性别" :value="user.gender === 0 ? '男' : '女'" is-link />
+    <van-cell
+      title="性别"
+      :value="user.gender === 0 ? '男' : '女'"
+      is-link
+      @click="isUpdateGenderShow = true"
+    />
     <van-cell title="生日" :value="user.birthday" is-link />
     <!-- /个人信息 -->
     <!-- 编辑昵称 -->
@@ -34,21 +39,33 @@
       />
     </van-popup>
     <!-- /编辑昵称 -->
+    <!-- 编辑性别 -->
+    <van-popup v-model="isUpdateGenderShow" position="bottom">
+      <update-gender
+        v-if="isUpdateGenderShow"
+        v-model="user.gender"
+        @close="isUpdateGenderShow = false"
+      />
+    </van-popup>
+    <!-- /编辑性别 -->
   </div>
 </template>
 
 <script>
 import { getUserProfile } from '@/api/user'
 import UpdateName from './components/update-name'
+import UpdateGender from './components/update-gender'
 export default {
   name: 'UserProfile',
   components: {
-    UpdateName
+    UpdateName,
+    UpdateGender
   },
   data() {
     return {
       user: {}, // 个人信息
-      isUpdateNameShow: false
+      isUpdateNameShow: false,
+      isUpdateGenderShow: false
     }
   },
   created() {

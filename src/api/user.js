621c0a802534c2d906f6a4413fe208a231b5108c
ifer-itcast <yangkangkang@itcast.cn>
2020-11-13 15:58:00 +0800

    展示用户信息

--- src/api/user.js
+++ src/api/user.js
@@ -55,3 +55,13 @@ export const deleteFollow = target => {
     url: `/app/v1_0/user/followings/${target}`
   })
 }
+
+/**
+ * 获取当前登录用户的个人资料
+ */
+export const getUserProfile = target => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user/profile'
+  })
+}
--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -10,29 +10,38 @@
     <!-- /导航栏 -->
     <!-- 个人信息 -->
     <van-cell title="头像" is-link>
-      <van-image
-        class="avatar"
-        fit="cover"
-        round
-        src="https://img.yzcdn.cn/vant/cat.jpeg"
-      />
+      <van-image class="avatar" fit="cover" round :src="user.photo" />
     </van-cell>
-    <van-cell title="昵称" value="内容" is-link />
-    <van-cell title="性别" value="内容" is-link />
-    <van-cell title="生日" value="内容" is-link />
+    <van-cell title="昵称" :value="user.name" is-link />
+    <van-cell title="性别" :value="user.gender === 0 ? '男' : '女'" is-link />
+    <van-cell title="生日" :value="user.birthday" is-link />
     <!-- /个人信息 -->
   </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/user'
 export default {
-  name: '',
+  name: 'UserProfile',
 
   data() {
-    return {}
+    return {
+      user: {} // 个人信息
+    }
   },
-
-  methods: {}
+  created() {
+    this.loadUserProfile()
+  },
+  methods: {
+    async loadUserProfile() {
+      try {
+        const { data } = await getUserProfile()
+        this.user = data.data
+      } catch (err) {
+        this.$toast('数据获取失败')
+      }
+    }
+  }
 }
 </script>
 

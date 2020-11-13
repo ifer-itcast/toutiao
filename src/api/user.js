bce574ab0c11fdf8c39948fd143f21b86aa9e4fb
ifer-itcast <yangkangkang@itcast.cn>
2020-11-13 16:50:49 +0800

    处理完成

--- src/api/user.js
+++ src/api/user.js
@@ -65,3 +65,14 @@ export const getUserProfile = target => {
     url: '/app/v1_0/user/profile'
   })
 }
+
+/**
+ * 更新用户资料
+ */
+export const updateUserProfile = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/profile',
+    data
+  })
+}
--- src/views/user-profile/components/update-name.vue
+++ src/views/user-profile/components/update-name.vue
@@ -6,6 +6,7 @@
       left-text="取消"
       right-text="完成"
       @click-left="$emit('close')"
+      @click-right="onConfirm"
     />
     <!-- /导航栏 -->
     <!-- 输入框 -->
@@ -25,6 +26,7 @@
 </template>
 
 <script>
+import { updateUserProfile } from '@/api/user'
 export default {
   name: 'UpdateName',
   props: {
@@ -39,7 +41,38 @@ export default {
     }
   },
 
-  methods: {}
+  methods: {
+    async onConfirm() {
+      this.$toast.loading({
+        message: '保存中...',
+        forbidClick: true, // 禁止背景点击
+        duration: 0 // 持续展示
+      })
+
+      try {
+        const localName = this.localName
+        if (!localName.length) {
+          this.$toast('昵称不能为空')
+          return
+        }
+
+        await updateUserProfile({
+          name: localName
+        })
+
+        // 更新视图
+        this.$emit('input', localName)
+
+        // 关闭弹层
+        this.$emit('close')
+
+        // 提示成功
+        this.$toast.success('更新成功')
+      } catch (err) {
+        this.$toast.fail('更新失败')
+      }
+    }
+  }
 }
 </script>
 

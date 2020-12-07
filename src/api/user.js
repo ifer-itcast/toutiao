b7b4d99720d8eeec7c77c2f146f0f60c12c3944e
ifer-itcast <yangkangkang@itcast.cn>
2020-12-07 12:55:35 +0800

    .

--- src/api/user.js
+++ src/api/user.js
@@ -4,7 +4,7 @@ import request from '@/utils/request'
 export const login = data => {
   return request({
     method: 'POST',
-    url: '/app/v1_0/authorizations',
+    url: '/v1_0/authorizations',
     data
   })
 }
@@ -13,6 +13,6 @@ export const login = data => {
 export const sendSms = mobile => {
   return request({
     method: 'GET',
-    url: `/app/v1_0/sms/codes/${mobile}`
+    url: `/v1_0/sms/codes/${mobile}`
   })
 }

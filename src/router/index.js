62ac94af54872e2b3c1df9a39568dbc8e2b48de0
ifer-itcast <yangkangkang@itcast.cn>
2020-12-20 14:45:15 +0800

    界面权限处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { Dialog } from 'vant'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -7,7 +9,10 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/login')
+    component: () => import('@/views/login'),
+    meta: {
+      requireAuth: false
+    }
   },
   {
     path: '/',
@@ -17,45 +22,98 @@ const routes = [
       {
         path: '', // 默认子路由，只能有一个
         name: 'home',
-        component: () => import('@/views/home')
+        component: () => import('@/views/home'),
+        meta: {
+          requireAuth: false
+        }
       },
       {
         path: '/qa',
         name: 'qa',
-        component: () => import('@/views/qa')
+        component: () => import('@/views/qa'),
+        meta: {
+          requireAuth: true
+        }
       },
       {
         path: '/video',
         name: 'video',
-        component: () => import('@/views/video')
+        component: () => import('@/views/video'),
+        meta: {
+          requireAuth: false
+        }
       },
       {
         path: '/my',
         name: 'my',
-        component: () => import('@/views/my')
+        component: () => import('@/views/my'),
+        meta: {
+          requireAuth: false
+        }
       }
     ]
   },
   {
     path: '/search',
     name: 'search',
-    component: () => import('@/views/search')
+    component: () => import('@/views/search'),
+    meta: {
+      requireAuth: false
+    }
   },
   {
     path: '/article/:articleId',
     name: 'article',
     component: () => import('@/views/article'),
-    props: true // 开启 props 传参，把路由参数映射到组件的 props 数据中
+    props: true, // 开启 props 传参，把路由参数映射到组件的 props 数据中
+    meta: {
+      requireAuth: false
+    }
   },
   {
     path: '/user/profile',
     name: 'user-profile',
-    component: () => import('@/views/user-profile')
+    component: () => import('@/views/user-profile'),
+    meta: {
+      requireAuth: false
+    }
   }
 ]
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
+
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    if (store.state.user) {
+      return next()
+    }
+    Dialog.confirm({
+      title: '提示',
+      message: '确认登录？'
+    })
+      .then(() => {
+        router.replace({
+          name: 'login',
+          query: {
+            redirect: router.currentRoute.fullPath
+          }
+        })
+      })
+      .catch(() => {
+        // Error: Navigation aborted from
+        next(false)
+      })
+  } else {
+    next()
+  }
 })
 
 export default router
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import store from '@/store'
 import JSONbig from 'json-bigint'
+import { Toast } from 'vant'
+import router from '@/router'
 
 const request = axios.create({
   // baseURL: 'http://ttapi.research.itcast.cn/', // 接口的基准路径
@@ -13,6 +15,8 @@ const request = axios.create({
     }
   }]
 })
+
+const refreshTokenReq = axios.create()
 
 // 请求拦截器
 request.interceptors.request.use(config => {
@@ -27,5 +31,58 @@ request.interceptors.request.use(config => {
   // 如果请求出错了（还没发出去）
   return Promise.reject(error)
 })
+
+request.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, async function (error) {
+  const status = error.response.status
+  if (status === 400) {
+    // 请求参数
+    Toast.fail('客户端请求参数错误')
+  } else if (status === 401) {
+    // token
+    const { user } = store.state
+    if (!user || !user.token) {
+      return redirectLogin()
+    }
+    try {
+      const { data } = await refreshTokenReq({
+        method: 'PUT',
+        url: '/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${user.refresh_token}`
+        }
+      })
+      user.token = data.data.token
+      store.commit('setUser', user)
+      console.dir(error)
+      return request(error.config)
+    } catch (err) {
+      console.log('23333')
+      return redirectLogin()
+    }
+  } else if (status === 403) {
+    Toast.fail('权限')
+    // 权限
+  } else if (status >= 500) {
+    // 服务端
+    Toast.fail('服务端异常')
+  }
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
+
+function redirectLogin() {
+  router.replace({
+    name: 'login',
+    query: {
+      // router.currentRoute => this.$route
+      redirect: router.currentRoute.fullPath
+    }
+  })
+}
 
 export default request

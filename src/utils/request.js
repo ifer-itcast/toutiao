cd3ce824d3ef763c0c357f2f3e852b1e384e577f
ifer-itcast <yangkangkang@itcast.cn>
2020-12-22 10:43:00 +0800

    统一错误处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import store from '@/store'
 import JSONbig from 'json-bigint'
+import { Toast } from 'vant'
 
 const request = axios.create({
   // baseURL: 'http://ttapi.research.itcast.cn/', // 接口的基准路径
@@ -25,6 +26,31 @@ request.interceptors.request.use(config => {
   return config
 }, error => {
   // 如果请求出错了（还没发出去）
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+request.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  const status = error.response.status
+  if (status === 400) {
+    Toast.fail('客户端请求参数错误')
+  } else if (status === 401) {
+    // 无效的 TOKEN
+    Toast.fail('无效的TOKEN')
+  } else if (status === 403) {
+    Toast.fail('客户端没有权限')
+  } else if (status === 404) {
+    Toast.fail('请求资源不存在')
+  } else if (status === 405) {
+    Toast.fail('请求方法不支持')
+  } else if (status >= 500) {
+    Toast.fail('服务器抽风了')
+  }
+  // 错误错误
   return Promise.reject(error)
 })
 

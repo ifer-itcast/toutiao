83df7238a229cfa42900d5e04299a6f478007796
ifer-itcast <yangkangkang@itcast.cn>
2021-02-24 19:45:07 +0800

    .

--- src/api/user.js
+++ src/api/user.js
@@ -5,7 +5,7 @@ import store from '@/store'
 export const login = data => {
   return request({
     method: 'POST',
-    url: '/v1_0/authorizations',
+    url: '/app/v1_0/authorizations',
     data
   })
 }
@@ -14,7 +14,7 @@ export const login = data => {
 export const sendSms = mobile => {
   return request({
     method: 'GET',
-    url: `/v1_0/sms/codes/${mobile}`
+    url: `/app/v1_0/sms/codes/${mobile}`
   })
 }
 
@@ -22,7 +22,7 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/v1_0/user',
+    url: '/app/v1_0/user',
     // 携带请求头信息
     headers: {
       Authorization: `Bearer ${store.state.user.token}`

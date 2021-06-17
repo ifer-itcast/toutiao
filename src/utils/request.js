1e4acbd4d2316482a8e559b7a7aac66f50b64850
ifer-itcast <yangkangkang@itcast.cn>
2021-06-17 16:55:19 +0800

    .

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/' // 接口的基准路径
+  // baseURL: 'http://ttapi.research.itcast.cn/' // 接口的基准路径
 })
 
 export default request

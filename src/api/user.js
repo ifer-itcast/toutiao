56c37194afbf1f79ea294edb437b9a218b9211ad
ifer-itcast <yangkangkang@itcast.cn>
2021-06-17 16:42:51 +0800

    .

--- src/api/user.js
+++ src/api/user.js
@@ -4,8 +4,7 @@ import request from '@/utils/request'
 export const login = data => {
   return request({
     method: 'POST',
-    // url: '/app/v1_0/authorizations',
-    url: '/v1_0/authorizations',
+    url: '/app/v1_0/authorizations',
     data
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const request = axios.create({
+  // 这里不要写
   // baseURL: 'http://ttapi.research.itcast.cn/' // 接口的基准路径
-  baseURL: 'http://toutiao-app.itheima.net/' // 接口的基准路径
+  // baseURL: 'http://toutiao-app.itheima.net/' // 接口的基准路径
 })
 
 export default request

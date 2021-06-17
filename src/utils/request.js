97086e5437642d4bfac2b69d8fcb36890f0e0c64
ifer-itcast <yangkangkang@itcast.cn>
2021-06-17 16:50:04 +0800

    .

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://toutiao-app.itheima.net/' // 接口的基准路径
+  // baseURL: 'http://toutiao-app.itheima.net/' // 接口的基准路径
 })
 
 export default request

4fa228e402ca718abb6f42043836513d6ae4c935
ifer-itcast <yangkangkang@itcast.cn>
2021-06-17 16:47:26 +0800

    .

--- src/api/user.js
+++ src/api/user.js
@@ -4,7 +4,7 @@ import request from '@/utils/request'
 export const login = data => {
   return request({
     method: 'POST',
-    url: '/v1_0/authorizations',
+    url: '/app/v1_0/authorizations',
     data
   })
 }

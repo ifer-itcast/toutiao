848f898b497e14398cd6579ad5ee234fe8ee3b13
ifer-itcast <yangkangkang@itcast.cn>
2021-06-17 18:02:30 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/app': {
-        target: 'http://toutiao-app.itheima.net/',
+        // target: 'http://toutiao-app.itheima.net/',
+        target: 'http://localhost:8000',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
